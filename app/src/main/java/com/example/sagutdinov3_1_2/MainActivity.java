25cafaeec6e48795ea8176223af4a4529030f937
Azat Sagutdinov <azat.sagutdinov@tele2med.ru>
2020-03-04 16:57:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sagutdinov3_1_2/MainActivity.java
@@ -0,0 +1,160 @@
+package com.example.sagutdinov3_1_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.sagutdinov3_1_2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView outputText;
+
+    Button butC, butPlusMinus, butProc, butDiv, butMul, butPlus, butMinus, butEq, butPoint;
+    String selected = "";
+    double data;
+    final String PLUS = "plus";
+    final String MINUS = "minus";
+    final String PROCENT = "procent";
+    final String MUL = "multiplex";
+    final String DIV = "div";
+
+    private void initViews() {
+
+        outputText = findViewById(R.id.outputText);
+        butC = findViewById(R.id.butC);
+        butPlus = findViewById(R.id.butPlus);
+        butMinus = findViewById(R.id.butMinus);
+        butMul = findViewById(R.id.butMul);
+        butDiv = findViewById(R.id.butDiv);
+        butEq = findViewById(R.id.butEq);
+        butProc = findViewById(R.id.butProc);
+        butPlusMinus = findViewById(R.id.butPlusMinus);
+        butPoint = findViewById(R.id.butPoint);
+
+        butPlusMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outputText.setText(String.valueOf((
+                        -1 * Double.valueOf(outputText.getText().toString()))));
+            }
+        });
+
+        butPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data = Double.valueOf(outputText.getText().toString());
+                outputText.setText("");
+                selected = PLUS;
+            }
+        });
+
+        butMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data = Double.valueOf(outputText.getText().toString());
+                outputText.setText("");
+                selected = MINUS;
+            }
+        });
+
+        butProc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data = Double.valueOf(outputText.getText().toString());
+                outputText.setText("");
+                selected = PROCENT;
+            }
+        });
+
+        butMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data = Double.valueOf(outputText.getText().toString());
+                outputText.setText("");
+                selected = MUL;
+            }
+        });
+
+        butDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data = Double.valueOf(outputText.getText().toString());
+                outputText.setText("");
+                selected = DIV;
+            }
+        });
+
+        butEq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!selected.equals("")) {
+                    double dataEq = Double.valueOf(outputText.getText().toString());
+                    switch (selected) {
+                        case PLUS: {
+                            outputText.setText(String.valueOf(data + dataEq));
+                            break;
+                        }
+                        case MINUS: {
+                            outputText.setText(String.valueOf(data - dataEq));
+                            break;
+                        }
+                        case PROCENT: {
+                            outputText.setText(String.valueOf(data / 100 * dataEq));
+                            break;
+                        }
+                        case MUL: {
+                            outputText.setText(String.valueOf(data * dataEq));
+                            break;
+                        }
+                        case DIV: {
+                            if (dataEq == 0) {
+                                Toast.makeText(MainActivity.this,
+                                        "Ошибка, 0", Toast.LENGTH_LONG).show();
+                            } else {
+                                outputText.setText(String.valueOf(data / dataEq));
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+        butC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selected = "";
+                outputText.setText("");
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+    }
+
+    public void btnPress(View view) {
+        boolean c = outputText.getText().toString().indexOf(",") == 1 ? true : false;
+        int a = c ? 1 : 0;
+        Button btn = (Button) view;
+        if ((btn.getId() == R.id.butPoint) && (a == 1)) {
+            Toast.makeText(MainActivity.this, "Ошибка, число уже не целое", Toast.LENGTH_LONG).show();
+        } else {
+            outputText.append(btn.getText());
+
+        }
+
+
+    }
+}
